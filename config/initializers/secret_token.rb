3cb25610a9cf46a7d4cb144555a88062e50f9cc0
Ubuntu 12.04 <neel@ubuntu.(none)>
2014-02-25 22:39:28 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoku::Application.config.secret_token = 'c71af54be7f88be468e06e933966eb08cd763f046447ab85c8c0e2a85a2127d955c0eb55a41c1922107f0ffbfc8be5bd034c464eb6db02b6b6cd38e7dd7f7f6d'
